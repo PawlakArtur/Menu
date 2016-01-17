70f143494b4e7380fb059f7707476433f02bcfa9
PawlakArtur <pawlak.artur90@gmail.com>
2016-01-17 17:59:03 +0100

    Menu

---
+++ zad3.js
@@ -0,0 +1,4 @@
+var developerAdd = document.createElement("li");
+var developer = document.getElementsByTagName("ul");
+developerAdd.innerText = "Developer";
+developer[0].appendChild(developerAdd);
